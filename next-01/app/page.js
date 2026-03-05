da9e489577e56878e5020bdddcea0d07cbb67bba
Giantrafa <giantrafa@gmail.com>
2026-03-05 23:42:34 +0000

    sexta

--- next-01/app/page.js
+++ next-01/app/page.js
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import * as components from "@components";
+
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      ola mundo
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      Olá, Turma!
+      <Profile />
+      Exemplo de expressão: {5 ** 3}
     </div>
   );
 }
